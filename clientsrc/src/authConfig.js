9c12b55d93902e2f4b749689d06bc162400bd884
JustinLGates <papagates1@gmail.com>
2020-06-30 10:19:48 -0600

    authConfigVars changed

--- clientsrc/src/authConfig.js
+++ clientsrc/src/authConfig.js
@@ -1,3 +1,3 @@
-export const domain = "devautho.auth0.com";
-export const audience = "https://devautho.auth0.com/api/v2/";
-export const clientId = "zJFW1LqSODTvujPrM1nEe37v4IbaR2NU";
+export const domain = "dev-2ygzkfxt.us.auth0.com";
+export const audience = "https://dev-2ygzkfxt.us.auth0.com/api/v2/";
+export const clientId = "2kpj0uy4ZkF0nlWR1OCZ2AQ4JjT6UUkQ";
